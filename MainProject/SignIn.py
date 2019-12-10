246510e9ce68569ba54c4fff67bfaf16c0a3f418
nguyenmarvin8 <nguyenmarvin8@gmail.com>
2019-12-09 18:40:16 -0800

    sign in

--- MainProject/SignIn.py
+++ MainProject/SignIn.py
@@ -1,6 +1,7 @@
 # Add your name on a line below
 
-# > Deanna M Cochener
+# > Marvin Nguyen
 #     > Soccer jersey collection app
 #     >API: api.football-data.org
 #     >Data scrape: https://www.mlssoccer.com/news
+#     >Ideas: Create a new color palette, new nav bar
