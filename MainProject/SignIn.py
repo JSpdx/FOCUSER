18d739f5ee29748e4c973361742310279dd6e7e1
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-03-02 15:22:21 -0800

    add Jon S sign in info

--- MainProject/SignIn.py
+++ MainProject/SignIn.py
@@ -5,4 +5,10 @@
 #     >API: api.football-data.org
 #     >Data scrape: https://www.mlssoccer.com/news
 
+# > Jon Shaw
+#   > Astronomy App. Database is a list of Solar Eclipse times and locations
+#   > API: I want to use one or more APIs from NASA, either NASA https://apod.nasa.gov/apod/astropix.html,
+#          DONKI (https://ccmc.gsfc.nasa.gov/donki/), or EPIC (https://epic.gsfc.nasa.gov/)
+#   > Data scrape: NASA ISS feed. https://www.nasa.gov/mission_pages/station/main/index.html
+
 
