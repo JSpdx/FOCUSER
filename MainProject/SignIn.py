3d2b839910080f408be9e92a2d36f617c41fa395
Bryce Braden <brycecbraden@gmail.com>
2020-03-03 02:01:44 -0600

    sign in

--- MainProject/SignIn.py
+++ MainProject/SignIn.py
@@ -1,8 +1,3 @@
 # Add your name on a line below
 
-# > Deanna M Cochener
-#     > Soccer jersey collection app
-#     >API: api.football-data.org
-#     >Data scrape: https://www.mlssoccer.com/news
-
-
+# > Bryce C Braden
