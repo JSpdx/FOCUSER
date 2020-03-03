ee399cb952633b977dda6930e23fcc1fba49cfb6
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-03-03 14:02:31 -0800

    add Eclipse model

--- MainProject/Focuser/models.py
+++ MainProject/Focuser/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+ECLIPSE_TYPE = (('Solar', 'Solar'), ('Lunar', 'Lunar'))
+ECLIPSE_SUBTYPE = (('Penumbral', 'Penumbral'), ('Annular', 'Annular'), ('Total', 'Total',), ('Partial', 'Partial'))
+
+class Eclipse(models.Model):
+    date = models.DateField(blank=True, null=True)
+    locations = models.CharField(max_length=50, blank=True)
+    type = models.CharField(max_length=20, choices= ECLIPSE_TYPE)
+    subtype = models.CharField(max_length=50, choices = ECLIPSE_SUBTYPE)
+    description = models.CharField(max_length=500, blank= True, null = True)
+
+    Eclipses = models.Manager()
+
+    def __str__(self):
+        return self.date
