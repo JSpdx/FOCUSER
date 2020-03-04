ee0b881e26f95f630f5c348f3112e636ed6a9daa
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-03-03 17:52:12 -0800

    Story 2 completed

--- MainProject/Focuser/models.py
+++ MainProject/Focuser/models.py
@@ -4,11 +4,11 @@ ECLIPSE_TYPE = (('Solar', 'Solar'), ('Lunar', 'Lunar'))
 ECLIPSE_SUBTYPE = (('Penumbral', 'Penumbral'), ('Annular', 'Annular'), ('Total', 'Total',), ('Partial', 'Partial'))
 
 class Eclipse(models.Model):
-    date = models.DateField(blank=True, null=True)
-    locations = models.CharField(max_length=50, blank=True)
+    date = models.CharField(max_length=20, blank=False, null=True)
+    locations = models.CharField(max_length=100, blank=True)
     type = models.CharField(max_length=20, choices= ECLIPSE_TYPE)
     subtype = models.CharField(max_length=50, choices = ECLIPSE_SUBTYPE)
-    description = models.CharField(max_length=500, blank= True, null = True)
+    description = models.CharField(max_length=1000, blank= True, null = True)
 
     Eclipses = models.Manager()
 
