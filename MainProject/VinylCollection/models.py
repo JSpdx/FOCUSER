1537d9d689643c9787bfd7f42eb8d43b3fe166d4
Miles <59800778+MilesHoover@users.noreply.github.com>
2020-03-04 11:48:02 -0500

    add VinylRecordsApp

--- MainProject/VinylCollection/models.py
+++ MainProject/VinylCollection/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Album(models.Model):
+    title = models.CharField(max_length=100, null=True)
+    artist = models.CharField(max_length=100)
+    albums = models.Manager()
+    #Add image later
--- MainProject/VinylCollection/views.py
+++ MainProject/VinylCollection/views.py
@@ -1,5 +1,17 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import AlbumForm
 
 # Create your views here.
 def vinyl_collection_home(request):
     return render(request, 'VinylCollection/VinylCollection_Home.html')
+
+def album_add_view(request):
+    if request.method == "POST":
+        form = AlbumForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    else:
+        form = AlbumForm()
+    return render(request, "VinylCollection/Album_Add.html", {'form': form})
