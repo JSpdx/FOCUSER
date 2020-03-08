be2b3fa0fb56eecaaa5bac858a98de866952e06b
Miles <59800778+MilesHoover@users.noreply.github.com>
2020-03-08 19:48:42 -0400

    add edit page, edit function, edit url

--- MainProject/VinylCollection/urls.py
+++ MainProject/VinylCollection/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.vinyl_collection_home, name='albumHome'),
     path('add', views.album_add_view, name='albumAdd'),
     path('list/', views.list, name='albumList'),
-    path('list/<int:pk>/details/', views.details, name='albumDetails'),
-    path('list/<int:pk>/details/<int:pk>/edit/', views.edit, name='albumEdit'),
+    path('<int:pk>/details/', views.details, name='albumDetails'),
+    path('<int:pk>/edit/', views.edit, name='albumEdit'),
     ]
