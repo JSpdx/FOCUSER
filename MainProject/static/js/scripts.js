07e4dbcc7cbcd8b4dd565836473d11008b661ac4
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-03-10 14:27:16 -0700

    pre - css experiments

--- MainProject/static/js/scripts.js
+++ MainProject/static/js/scripts.js
@@ -1,10 +1,14 @@
 
+
+/*==============================JS for FOCUSER==================================*/
 var focuserBg =
 document.getElementsByClassName('focuser_background')[0];
 console.log(focuserBg);
 
-
+/* Function is called on load. It's placed in the body tag of templates/base.html */
 function focus() {
     focuserBg.classList.add('focused');
     console.log("asdasd");
 }
+
+/*============================== END JS for FOCUSER==================================*/
