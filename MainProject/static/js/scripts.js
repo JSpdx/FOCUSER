7508bf8df619fe2db27b5091643c23be21afea13
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-03-10 15:44:15 -0700

    add APOD styling

--- MainProject/static/js/scripts.js
+++ MainProject/static/js/scripts.js
@@ -3,12 +3,19 @@
 /*==============================JS for FOCUSER==================================*/
 var focuserBg =
 document.getElementsByClassName('focuser_background')[0];
+var apod_img =
+document.getElementsByClassName('apod_img')[0];
 console.log(focuserBg);
 
 /* Function is called on load. It's placed in the body tag of templates/base.html */
 function focus() {
+    if (focuserBg != null) {
     focuserBg.classList.add('focused');
+    }
+    if (apod_img != null) {
+    apod_img.classList.add('apod_img_transition');
     console.log("asdasd");
+    }
 }
 
 /*============================== END JS for FOCUSER==================================*/
