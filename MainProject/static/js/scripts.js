3b3bfa80e39be1310cda5805de1b711534bc6db3
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-03-10 15:57:36 -0700

    add front page quote

--- MainProject/static/js/scripts.js
+++ MainProject/static/js/scripts.js
@@ -5,11 +5,11 @@ var focuserBg =
 document.getElementsByClassName('focuser_background')[0];
 var apod_img =
 document.getElementsByClassName('apod_img')[0];
-console.log(focuserBg);
+
 
 /* Function is called on load. It's placed in the body tag of templates/base.html */
 function focus() {
-    if (focuserBg != null) {
+    if (focuserBg != null) {        /* these IF statements keep errors from being thrown. The variable is null if the class name didn't exist on that page */
     focuserBg.classList.add('focused');
     }
     if (apod_img != null) {
